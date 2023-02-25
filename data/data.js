9b5ae291057dcf911adba4a90952f0e23d5c55ba
ErnestoGZ31 <ernesto_1322134235@uptecamac.edu.mx>
2023-02-24 19:34:48 -0600

    Quiz

---
+++ data/data.js
@@ -0,0 +1,53 @@
+export const data = [
+
+{
+    question: "When was born ?",
+    choices: ["november 11, 2000", "junary 10, 2001", "December 10, 2000", "september 15,2000"],
+    answer: "December 10, 2000",
+  },
+  {
+    question:"Who are your parents?",
+    choices: ["her mother is aurora and her father is alfredo", "his mom is Martha  and her dad is Evaristo", "her mother is avilene and her father is salvador", "her mother is shakira and her father is pique","he doesn't know his parents"],
+    answer: "his mom is Martha  and her dad is Evaristo",
+  },
+  {
+    question: "How many brothers are there in total?",
+    choices: ["there are two", "there are for", "only her", "There are three"],
+    answer: "There are three",
+  },
+  {
+    question: " What anecdote do you have?",
+    choices: ["fall in public", "adopt a pet", "When he dressed up as a pumpkin at a costume party", "had an accident"],
+    answer: "When he dressed up as a pumpkin at a costume party",
+  },        
+  {
+    question: "What career are you studying?",
+    choices: ["Bachelor of International Business", "psychology", "biology", "engineering"],
+    answer: "Bachelor of International Business",
+  },
+  {
+    question: "What idea did you propose to your classmates?",
+    choices: ["sell drugs", "Create a michelada company", "burger stand", "only fans"],
+    answer: "Create a michelada company",
+  },
+  {
+    question: " What country have you visited? ",
+    choices: ["Poland, China", "Brasil", "italy and egypt", "France, Mexico, the United States, Spain"],
+    answer: "France, Mexico, the United States, Spain",
+  },
+  {
+    question: "How old are you?",
+    choices: ["Twenty two years", "thirty-two", "fifteen", "eighteen"],
+    answer: "Twenty two years",
+  },
+  {
+    question: " In which country was Juliet lost?",
+    choices: ["in south africa", "on the new york train", "In France, at the train station", "in Norway"],
+    answer: "In France, at the train station",
+  },
+  {
+    question: "How do you describe your life?",
+    choices: ["boring nonsense", "A mixture of laughter and madness", "stressful", "sad"],
+    answer: "A mixture of laughter and madness",
+  },
+]
